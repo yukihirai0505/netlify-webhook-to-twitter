6d50b58f9f7af818732d5dbd4d6f9b4ce06410a7
Yuki Hirai <yukihirai0505@gmail.com>
2019-08-04 12:17:32 +0900

    Add netlify webhook to twitter

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+const axios = require('axios')
 const twitter = require('twitter')
 const { json, run } = require('micro')
 
@@ -19,6 +20,32 @@ const client = new twitter({
 module.exports = async (req, res) => {
   run(req, res, async request => {
     const payload = await json(request)
+    const title = payload['title']
+    if (title.includes('Add a new article for postId')) {
+      const commitURL = payload['commit_url']
+        .replace('github.com', 'api.github.com/repos')
+        .replace('commit', 'commits')
+      const response = await axios.get(commitURL)
+      const files = response.data.files
+      if (files) {
+        const file = files[0]
+
+        const newUrl = file.filename
+          .replace('src/pages', 'https://pr.yabaiwebyasan.com')
+          .replace('.md', '/')
+
+        const message = `【新着ステマ！】\n\n新着取材です！\n是非ご覧ください！\n\n${newUrl}\n\n#PR #ステマ`
+        console.log(message)
+        const response = await client
+          .post('statuses/update', {
+            status: message,
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        console.log(response)
+      }
+    }
     console.info(payload)
     return moment().format('MMMM Do YYYY, h:mm:ss a')
   })
